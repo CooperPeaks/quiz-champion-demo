41120ab6f2bc803f4d0c10e42546b9d9db2bac12
CooperPeaks <jbayard269@gmail.com>
2024-02-10 17:13:00 +0100

    js update

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-let pseudoForm = document.getElementById("pseudo")
+let button_start = document.getElementById("startButton")
+button_start.addEventListener("click", ()=>{
+    console.log("hello")
+    // Récupération input pseudo
+    let pseudo_form = document.getElementById("pseudo")
+    let pseudo_value = pseudo_form.value 
+    if(pseudo_value.length <= 0) {
+        console.log("world")
+        alert("Veuillez choisir un pseudo pour commencer la partie")
+    }
+    else{
+        console.log("back")
+        window.location.href = "game.html"
+        // ! Récupérer la valeur de pseudo pour qu'elle soit conservée durant la partie (utiliser localStorage ?)
+    }
+})
 
-function on() {
-    let pseudoValue = pseudoForm.value
-    if(pseudoValue.length < 5) {
-        console.log("success 1")
-    }
-    else {
-        console.log("succes 2")
-    }
-}
