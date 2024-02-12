fbb16960bc9241b27a2808622db9f6595b9d338b
CooperPeaks <jbayard269@gmail.com>
2024-02-12 12:07:51 +0100

    js create card function

--- test/game.js
+++ test/game.js
@@ -2,7 +2,7 @@ const button = document.getElementById('button')
 const main = document.getElementById('main')
 
 const mainCard = document.getElementById('front_card')
-
+const backCard = document.getElementById('card')
 
 button.addEventListener('click', () => {
     console.log('hello')
@@ -14,12 +14,58 @@ button.addEventListener('click', () => {
     const newCard = document.createElement('div')
     newCard.innerHTML = `
             <h1>${titre}</h1>
-            <button>${questButton1}</button>
-            <button>${questButton2}</button>
-            <button>${questButton3}</button>
+            <button class='questButton' id='difficult'>${questButton1}</button>
+            <button class='questButton' id='medium'>${questButton2}</button>
+            <button class='questButton' id='easy'>${questButton3}</button>
         `;
     newCard.id = ('card')
     main.appendChild(newCard)
     mainCard.remove()
+
+    // Manipuler new card
+    const questButton = newCard.querySelectorAll('.questButton')
+    // Créer une loop pour effectuer même action peu importe l'input
+    questButton.forEach(button => {
+        button.addEventListener('click', () => {
+            console.log("click")
+            // Récupérer input pour les manipuler plus tard
+            const difficult = document.getElementById('difficult')
+            const medium = document.getElementById('medium')
+            const easy = document.getElementById('easy')
+            // Conditions
+            if (button === difficult) {
+                console.log('click2')
+                // Retirer les inputs
+                difficult.remove()
+                medium.remove()
+                easy.remove()
+                // Faire apparaître la question
+                const question = document.createElement('div')
+                const questiontxt = "Voulez-vous acheter des NFTs ?"
+                question.textContent = questiontxt
+                newCard.appendChild(question)
+                createAnswers()
+            }
+        })
+    })
 })
 
+function createAnswers() {
+    const position = document.getElementById('anBloc')
+    const answer1 = "Yes"
+    const answer2 = "No"
+    const answerBloc = document.createElement('div')
+    answerBloc.innerHTML = `
+        <div class="an1">
+            <button class='yes' id='yes'></button>
+            <p>${answer1}</p>
+        </div>
+
+        <div class="an2">
+            <button class='no' id='no'></button>
+            <p>${answer2}</p>
+        </div>
+    `;
+    answerBloc.id = ('random')
+    position.appendChild(answerBloc)
+}
