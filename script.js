42642c35c9f447b649501acbd6e4367d7b4678a0
CooperPeaks <jbayard269@gmail.com>
2024-02-22 14:04:13 +0100

    flip-card 3d effect

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-let button_start = document.getElementById("startButton")
-button_start.addEventListener("click", ()=>{
-    console.log("hello")
-    // Récupération input pseudo
-    let pseudo_form = document.getElementById("pseudo")
-    let pseudo_value = pseudo_form.value 
-    if(pseudo_value.length <= 0) {
-        console.log("world")
-        alert("Veuillez choisir un pseudo pour commencer la partie")
-    }
-    else{
-        console.log("back")
-        window.location.href = "game.html"
-        // ! Récupérer la valeur de pseudo pour qu'elle soit conservée durant la partie (utiliser localStorage ?)
-    }
-})
+function flipCard() {
+    var flipCard = document.querySelector('.flip-card');
+    flipCard.classList.add('flipped');
+}
 
+function flipCardBack() {
+    var flipCard = document.querySelector('.flip-card');
+    flipCard.classList.remove('flipped');
+}
